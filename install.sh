6bb889bae2e371ca34680f7973d528bd9f5047d1
bstil22 <brady.stilwell@csaa.com>
2016-02-13 20:40:25 -0700

    add chmod for git-handy

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "installing git-handy"
 case "$OSTYPE" in
-  darwin*)  cp ./git-handy /usr/local/bin ;;
-  linux*)   cp ./git-handy /usr/bin ;;
+  darwin*)  cp ./git-handy /usr/local/bin; sudo chmod +x /usr/local/bin/git-handy ;;
+  linux*)   cp ./git-handy /usr/bin; sudo chmod +x /usr/bin/git-handy ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
 echo "done"
